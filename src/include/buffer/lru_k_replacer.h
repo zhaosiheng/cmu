94510169b5e4e675961862e0f0e89b4193fe6bba
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-09-21 21:44:24 +0800

    Update lru_k_replacer.h

--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -139,7 +139,7 @@ class LRUKReplacer {
   [[maybe_unused]] size_t curr_size_{0};
   [[maybe_unused]] size_t replacer_size_;
   [[maybe_unused]] size_t k_;
-  std::vector<std::pair<int,bool>> container;
+  std::vector<std::pair<int,bool>> my_buffer_;
   std::mutex latch_;
 };
 
