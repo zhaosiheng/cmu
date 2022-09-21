7d25f86cd95329ef09db4bf6caec792bfc85e91d
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-09-21 22:34:57 +0800

    9_21_22:34

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -18,12 +18,44 @@ LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_fra
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+    my_buffer_.push_back({frame_id , true});
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+    //find in buffer
+    auto it = find_if(my_buffer_.begin() , my_buffer_.end() , [frame_id](std::pair<frame_id_t , bool> tm)->bool{ return tm.first == frame_id;});
+    if(it == my_buffer_.end()){
+        //find in buffer_k
+        it = find_if(my_buffer_k_.begin() , my_buffer_k_.end() , [frame_id](std::pair<frame_id_t , bool> tm)->bool{ return tm.first == frame_id;});
+        if(it == my_buffer_.end()){
+            std::cout<<"do not find frame_id";
+            std::abort();
+        }
+    }
+    if(it->second && !set_evictable){
+        curr_size_--;
+    }else if(!it->second && set_evictable){
+        curr_size_++;
+    }
+    it->second = set_evictable;
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+    //find in buffer
+    auto it = find_if(my_buffer_.begin() , my_buffer_.end() , [frame_id](std::pair<frame_id_t , bool> tm)->bool{ return tm.first == frame_id;});
+    if(it == my_buffer_.end()){
+        //find in buffer_k
+        it = find_if(my_buffer_k_.begin() , my_buffer_k_.end() , [frame_id](std::pair<frame_id_t , bool> tm)->bool{ return tm.first == frame_id;});
+        if(it == my_buffer_.end()) return;
+    }
+    if(it->second){
+        curr_size_--;
+    }
+    my_buffer_.erase(it);
+    current_timestamp_--;
+}
 
-auto LRUKReplacer::Size() -> size_t { return current_timestamp_; }
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
