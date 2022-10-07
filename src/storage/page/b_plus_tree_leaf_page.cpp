31d15f39dbbddfa68adc4cf9990c2a42c7e94dc5
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2022-10-07 22:10:47 +0800

    22:10

--- src/storage/page/b_plus_tree_leaf_page.cpp
+++ src/storage/page/b_plus_tree_leaf_page.cpp
@@ -14,7 +14,7 @@
 #include "common/exception.h"
 #include "common/rid.h"
 #include "storage/page/b_plus_tree_leaf_page.h"
-#include "storage/index/b_plus_tree.h"
+
 namespace bustub {
 
 /*****************************************************************************
