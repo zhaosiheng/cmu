8e27c5aafc7d4c40e8d171e7bbd44041c931fb0a
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2023-01-10 19:29:02 +0800

    0

--- src/storage/page/b_plus_tree_leaf_page.cpp
+++ src/storage/page/b_plus_tree_leaf_page.cpp
@@ -95,7 +95,7 @@ bool B_PLUS_TREE_LEAF_PAGE_TYPE::insert(const KeyType &key, const ValueType &val
   }
   array_[pos].first = key;
   array_[pos].second = value;
-  LOG_INFO("# add a kv in leaf");
+  LOG_DEBUG("# add a kv=%d in leaf", key);
   
   if(GetSize() > GetMaxSize()){/*out of maxsize*/
     BPlusTreePage* page = tree->pid_to_page(GetParentPageId());
